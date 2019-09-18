81efd8d05a16f56fff86182cd71d27e05ee754de
Nandan Doddihal <nadoddi@iu.edu>
2019-09-18 13:16:15 -0400

    Event handling

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,14 +1,37 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
-  },
+    patients: [],
+      formPatient: {
+        firstName:'',
+        lastName:'',
+        dob:'',
+        sexAtBirth:''
+      };
+    },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
     }
+    handleCreateRecord(event){
+      // TODO: Post data to API
+      // fetch(url, {method:'post', data:this.formPatient})
+      // .then()
+      this.patients.push(this.formPatient);
+      this.formPatient = {
+          firstName:'',
+          lastName:'',
+          dob:'',
+          sexAtBirth:''
+        }
+    },
+    handleRecordClick(patient){
+      //TODO: Past the data to another Vue App
+      console.log(patient);
+      }
+
   },
   created() {
     this.fetchPatients();
