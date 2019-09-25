9b80b21eeb54327b934fdc45ba3c2ff4b4a40092
Nandan Doddihal <nadoddi@iu.edu>
2019-09-25 15:24:53 -0400

    Adding php files for records and waiting

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -5,7 +5,7 @@ var waitingApp = new Vue({
   },
   methods: {
     fetchPatients() {
-      fetch('dummy.php')
+      fetch('api/waiting/index.php')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
     }
