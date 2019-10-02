2b7eab79a74ad97789896fe5501e0291b4334a10
Nandan Doddihal <nadoddi@iu.edu>
2019-10-02 10:05:15 -0400

    10-02

--- app/public/js/triage.app.js
+++ app/public/js/triage.app.js
@@ -7,13 +7,21 @@ var patientTriageApp = new Vue({
     handleSubmit() {
       // TODO: Add the correct date via Javascript before posting
 
-       // TODO:
-       // fetch(url, {
-       //   method:'post',
-       //   data: this.patient
-       // })
-       // .then( ... )
-       waitingApp.patients.push(this.patient);
+       TODO:
+       fetch('api/waiting/post.php', {
+        method:'post',
+        body: JSON.stringify(this.patient)
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+       })
+       .then ( response => response.json() )
+       .then ( json => {waitingApp.patients = json})
+       .catch(err => {
+         console.error('TRIAGE POST ERROR:');
+         console.error(err);
+         });
+       // waitingApp.patients.push(this.patient);
        this.handleReset();
     },
     handleReset() {
