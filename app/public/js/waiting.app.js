420209fd712f7c07457aece72ca1ca6c22696d56
Nandan Doddihal <nadoddi@iu.edu>
2019-10-14 11:22:35 -0400

    Date and Time

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -8,6 +8,12 @@ var waitingApp = new Vue({
       fetch('api/waiting/index.php')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
+    },
+    displayWaitingLocalDate(d){
+      return moment.utc(d).local().format("HH:mm MMM Do");
+    },
+    displayWaitingSince(d) {
+      return moment.utc(d).local().fromNow("m");
     }
   },
   created() {
